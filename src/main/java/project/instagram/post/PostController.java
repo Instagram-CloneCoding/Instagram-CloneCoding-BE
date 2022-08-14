d714cd5949b4db91e2350d22b373588f1e164b13
littlecold4 <minu09252@gmail.com>
2022-08-14 19:45:42 +0900

    post Setup

---
+++ src/main/java/project/instagram/post/PostController.java
@@ -0,0 +1,9 @@
+package project.instagram.post;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class PostController {
+}
